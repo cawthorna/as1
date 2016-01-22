b76601b372ab88e71a304c4649c0efed56ba8da2
Adam Cawthorn <adamtcawt@gmail.com>
2016-01-22 10:45:46 -0700

    Implemented Gson over ObjectStream for loading and storing the fuel log.

--- src/cawthorn-FuelTracker/app/src/main/java/com/example/adam/cawthorn_fueltracker/MainActivity.java
+++ src/cawthorn-FuelTracker/app/src/main/java/com/example/adam/cawthorn_fueltracker/MainActivity.java
@@ -1,30 +1,30 @@
 package com.example.adam.cawthorn_fueltracker;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Iterator;
-import java.util.ListIterator;
-
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -49,13 +49,7 @@ public class MainActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_main);
 
-        loadFuelLog();
-
         listView = (ListView) findViewById(R.id.listview);
-
-        adapter = new ListViewArrayAdapter(this,FuelLogList);
-
-        listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -67,10 +61,21 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        loadFuelLog();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        saveFuelLog();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return true;
     }
 
@@ -97,7 +102,6 @@ public class MainActivity extends ActionBarActivity {
             //TODO: remove this.
             loadFuelLog();
             adapter.notifyDataSetChanged();
-            listView.invalidateViews();
             Toast.makeText(this,"View Refreshed",Toast.LENGTH_SHORT).show();
         }
 
@@ -107,8 +111,8 @@ public class MainActivity extends ActionBarActivity {
             if(file.exists()) file.delete();
             loadFuelLog();
             loadFuelLog();
+
             adapter.notifyDataSetChanged();
-            listView.invalidateViews();
             Toast.makeText(this, "File rebuilt", Toast.LENGTH_SHORT).show();
         }
 
@@ -150,7 +154,6 @@ public class MainActivity extends ActionBarActivity {
         updateHeader();
         saveFuelLog();
         adapter.notifyDataSetChanged();
-        listView.invalidateViews();
     }
 
     private void loadFuelLog() {
@@ -159,69 +162,51 @@ public class MainActivity extends ActionBarActivity {
         total_amount = 0;
 
         try {
-            File file = new File(getFilesDir(), FUEL_LOG_STORE);
-            if(!file.exists()) throw new FileNotFoundException();
-            FileInputStream fileInputStream = new FileInputStream(file);
-            ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
-            try {
-                FuelLogEntry fuelLogEntry;
-                while ((fuelLogEntry = (FuelLogEntry) inputStream.readObject()) != null) {
-                    FuelLogList.add(fuelLogEntry);
-                    total_amount += fuelLogEntry.getIntAmount();
-                    total_cost += fuelLogEntry.getIntCost();
-                }
-            } catch (EOFException ex) {
-                //do nothing, done reading in from file.
-            }
-            inputStream.close();
+
+            FileInputStream fileInputStream = openFileInput(FUEL_LOG_STORE);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            Gson gson = new Gson();
+            // below from http://google-gson.googlecode.com/svn/tags/1.1.1/docs/javadocs/com/google/gson/reflect/TypeToken.html
+            FuelLogList = gson.fromJson(bufferedReader, new TypeToken<ArrayList<FuelLogEntry>>() {}.getType());
+            Toast.makeText(MainActivity.this,"Length: " + FuelLogList.size(), Toast.LENGTH_SHORT).show();
+            bufferedReader.close();
             fileInputStream.close();
+
+
+
         } catch (FileNotFoundException ex) {
+
             // The file does not exist. create it and return an empty list.
             // should only happen first run.
             // add temporary objects for testing TODO: remove this
             System.out.println("File not found, creating new one. " + FUEL_LOG_STORE);
-            for(int i=0; i < 16; i++) {
-                FuelLogList.add(new FuelLogEntry(2000+i,2,3,"Abc",200000.2+i,"regular", 2, 100));
+            for (int i = 0; i < 16; i++) {
+                FuelLogList.add(new FuelLogEntry(2000 + i, 2, 3, "Abc", 200000.2 + i, "regular", 2, 100));
             }
+            saveFuelLog();
 
-            File file = new File(getFilesDir(), FUEL_LOG_STORE);
-            try {
-                FileOutputStream outputStream = new FileOutputStream(file);
-                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-                ListIterator<FuelLogEntry> iterator = FuelLogList.listIterator();
-                while(iterator.hasNext()) {
-                    objectOutputStream.writeObject(iterator.next());
-                }
-                objectOutputStream.close();
-                outputStream.close();
+        } catch(IOException  ex) {
+            throw new RuntimeException();
 
-            } catch (IOException exc) {
-                exc.printStackTrace();
-            }
-
-        } catch (IOException ex) {
-            System.out.println("IO Exception");
-            ex.printStackTrace();
-
-        } catch (ClassNotFoundException ex) {
-            System.out.println("FuelLogEntry class not found");
-            ex.printStackTrace();
         } finally {
+
             updateHeader();
             // Collections.sort(FuelLogList); Do not sort in accordance with assignment spec
+
+            adapter = new ListViewArrayAdapter(this,FuelLogList);
+            listView.setAdapter(adapter);
+
+            Toast.makeText(MainActivity.this, "returning from load fuel log with " + FuelLogList.size() + " entries.",Toast.LENGTH_SHORT).show();
         }
     }
 
     private void saveFuelLog() {
 
         try {
-            File file = new File(getFilesDir(), FUEL_LOG_STORE);
-            FileOutputStream fileOutputStream = new FileOutputStream(file, false);
-            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
-            ListIterator<FuelLogEntry> iterator = FuelLogList.listIterator();
-            while(iterator.hasNext()) {
-                outputStream.writeObject(iterator.next());
-            }
+            FileOutputStream fileOutputStream = openFileOutput(FUEL_LOG_STORE, MODE_PRIVATE);
+            BufferedWriter outputStream = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            Gson gson = new Gson();
+            gson.toJson(FuelLogList,outputStream);
             outputStream.close();
             fileOutputStream.close();
 
@@ -247,7 +232,7 @@ public class MainActivity extends ActionBarActivity {
         startActivityForResult(intent, EDIT_FUEL_ENTRY_OBJECT);
     }
 
-    public void updateHeader() {
+    private void updateHeader() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(total_amount / 1000);
         stringBuilder.append('.');
