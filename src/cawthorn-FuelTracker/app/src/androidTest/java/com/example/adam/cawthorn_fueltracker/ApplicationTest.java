9b32f9b55000d0ec0941439c16cbe3cba5b7a22b
Adam Cawthorn <adamtcawt@gmail.com>
2016-01-29 09:05:45 -0700

    Added unit test skeletons

--- src/cawthorn-FuelTracker/app/src/androidTest/java/com/example/adam/cawthorn_fueltracker/ApplicationTest.java
+++ src/cawthorn-FuelTracker/app/src/androidTest/java/com/example/adam/cawthorn_fueltracker/ApplicationTest.java
@@ -10,4 +10,125 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    /*
+     *  MainActivity Tests
+     */
+    public void loadFuelLogTest() {
+
+    }
+
+    public void saveFuelLogTest() {
+
+    }
+
+    public void updateHeaderTest() {
+
+    }
+
+    public void addSampleDataTest() {
+
+    }
+
+    public void sortFuelLogListTest() {
+
+    }
+
+    /*
+     * editEntry Tests
+     */
+    public void confirmAdditionTest() {
+
+    }
+
+    public void cancelAdditionTest() {
+
+    }
+
+    public void onDateSetTest() {
+
+    }
+
+    /*
+     * FuelLogEntry Tests
+     */
+    public void setDateTest() {
+
+    }
+
+    public void setYearTest() {
+
+    }
+
+    public void setMonthTest() {
+
+    }
+
+    public void setDayTest() {
+
+    }
+
+    public void setStation() {
+
+    }
+
+    public void setOdometerTest() {
+
+    }
+
+    public void setGradeTest() {
+
+    }
+
+    public void setAmountTest() {
+
+    }
+
+    public void setUnitCostTest() {
+
+    }
+
+    public void getDateTest() {
+
+    }
+
+    public void getFormattedOdometerTest() {
+
+    }
+
+    public void getOdometerTest() {
+
+    }
+
+    public void getFormattedAmountTest() {
+
+    }
+
+    public void getAmountTest() {
+
+    }
+
+    public void getFormattedUnitCostTest() {
+
+    }
+
+    public void getUnitCostTest() {
+
+    }
+
+    public void getCostTest() {
+
+    }
+
+    public void updateCostTest() {
+
+    }
+
+    public void checkCostTest() {
+
+    }
+
+    public void compareToTest() {
+
+    }
 }
